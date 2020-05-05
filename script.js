823876cf73dd560bfa6279f8edd8e289fb715694
Mateusz <chomamateusz@gmail.com>
2020-05-05 09:33:32 +0200

    Automatic commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers)
+
+const numbersInString = numbers.join(',')
+
+console.log(numbersInString)
+
+const numbersFromString = numbersInString.split(',')
+
+console.log(numbersFromString)
